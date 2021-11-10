45924cfd4a588d500c04a518185cb96bf388fd5e
bsanakoev <bsanakoev@notissimus.com>
2021-11-10 11:46:41 +0300

    first comit

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,14 +1,14 @@
-import ArticleList from './ArticleList';
+import ArticleList from '../ArticleList';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import agent from '../agent';
+import agent from '../../agent';
 import { connect } from 'react-redux';
 import {
   FOLLOW_USER,
   UNFOLLOW_USER,
   PROFILE_PAGE_LOADED,
   PROFILE_PAGE_UNLOADED
-} from '../constants/actionTypes';
+} from '../../constants/actionTypes';
 
 const EditProfileSettings = props => {
   if (props.isUser) {
