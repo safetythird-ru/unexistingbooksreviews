503d201540750f3fee824881e557cba9704c9b94
reachoutfaith <aromenskaja@gmail.com>
2021-11-15 13:38:28 +0300

    upadated CommentInput

--- src/components/Article/CommentInput.js
+++ src/components/Article/CommentInput.js
@@ -1,58 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import agent from '../../agent';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { ADD_COMMENT } from '../../constants/actionTypes';
+import { on } from 'superagent';
 
-const mapDispatchToProps = dispatch => ({
-  onSubmit: payload =>
-    dispatch({ type: ADD_COMMENT, payload })
-});
+const CommentInput = (props) => {
+  const [body, setBody] = useState('');
+  const dispatch = useDispatch();
 
-class CommentInput extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      body: ''
-    };
-
-    this.setBody = ev => {
-      this.setState({ body: ev.target.value });
-    };
-
-    this.createComment = ev => {
-      ev.preventDefault();
-      const payload = agent.Comments.create(this.props.slug,
-        { body: this.state.body });
-      this.setState({ body: '' });
-      this.props.onSubmit(payload);
-    };
+  const onChange = (ev) => {
+    setBody(ev.target.value);
   }
 
-  render() {
-    return (
-      <form className="card comment-form" onSubmit={this.createComment}>
-        <div className="card-block">
-          <textarea className="form-control"
-            placeholder="Write a comment..."
-            value={this.state.body}
-            onChange={this.setBody}
-            rows="3">
-          </textarea>
-        </div>
-        <div className="card-footer">
-          <img
-            src={this.props.currentUser.image}
-            className="comment-author-img"
-            alt={this.props.currentUser.username} />
-          <button
-            className="btn btn-sm btn-primary"
-            type="submit">
-            Post Comment
-          </button>
-        </div>
-      </form>
-    );
+  const onSubmit = (payload) => {
+    dispatch({
+      type: ADD_COMMENT,
+      payload
+    })
   }
+
+  const createComment = (ev) => {
+    ev.preventDefault();
+    console.log(props.slug, { body: body })
+    const payload = agent.Comments.create(props.slug,
+      { body: body });
+    onSubmit(payload);
+    setBody('')
+  }
+
+  return (
+    <form className="card comment-form" onSubmit={createComment}>
+      <div className="card-block">
+        <textarea className="form-control"
+          placeholder="Write a comment..."
+          value={body}
+          onChange={onChange}
+          rows="3">
+        </textarea>
+      </div>
+      <div className="card-footer">
+        <img
+          src={props.currentUser.image}
+          className="comment-author-img"
+          alt={props.currentUser.username} />
+        <button
+          className="btn btn-sm btn-primary"
+          type="submit">
+          Post Comment
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default connect(() => ({}), mapDispatchToProps)(CommentInput);
+export default CommentInput;
