561a401409d5a9176f0d31c9822627dd7dfb2f6c
Igor Yagushevsky <yagushevskij@gmail.com>
2021-12-11 20:57:50 +0300

    made textarea tag valid

--- src/components/field/text-area/text-area.js
+++ src/components/field/text-area/text-area.js
@@ -1,15 +1,13 @@
 import textArea from './text-area.module.css';
 import Field from '../field';
 
-const TextArea = ({edited, labelName, name, errorText, ...props }) => {
-  const hasError = edited && errorText
+const TextArea = ({ edited, labelName, name, value, errorText, ...props }) => {
+  const hasError = edited && errorText;
   return (
     <Field name={name} labelName={labelName} errorText={errorText} hasError={hasError}>
-        <textarea
-          className={`${textArea.textarea} ${hasError && textArea.textarea_error}`}
-          name={name}
-          {...props}
-        />
+      <textarea className={`${textArea.textarea} ${hasError && textArea.textarea_error}`} name={name} {...props}>
+        {value}
+      </textarea>
     </Field>
   );
 };
